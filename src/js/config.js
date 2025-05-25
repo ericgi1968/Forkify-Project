7d6c5a35baf5449a16e1f404fdeca2e4d21f9e96
Eric Gi <ericgi@hotmail.com>
2025-05-25 11:44:45 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "189de691-8315-4f57-91d9-d438dbe927a0";
+export const MODAL_CLOSE_SEC = 2.5;
